9570e2154baeb4bccfcd77dd63bf24af52bfaf43
Nadun Bothota <d.nadunbothota@gmail.com>
2025-10-10 10:31:36 +0530

    Update EC2Test.java

--- java/com/cloud/EC2Test.java
+++ java/com/cloud/EC2Test.java
@@ -6,8 +6,9 @@ public class EC2Test {
 
     public String testInstanceStartCommand(String s) {
         EC2Test manager = new EC2Test();
-        String response = manager.testInstanceStartCommand("i-12345678"); // mock this if it uses real AWS
+        String response = manager.testInstanceStartCommand("i-12345678");
         assertNotNull(response);
         return response;
     }
 }
+
